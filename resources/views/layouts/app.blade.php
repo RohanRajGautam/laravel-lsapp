0fdc437c185cee617a18593bc2ed0ddb098ef18f
rohan <rohanrajgautam@gmail.com>
2020-09-24 10:46:13 +0545

    added bootstrap navbar, jumbotron

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
     <title>{{env('APP_NAME')}}</title>
     </head>
     <body >
-      @yield('content')
+      @include('inc.navbar')
+      <div class="container">
+
+        @yield('content')
+      </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')   
-<h1>{{$title}}</h1>
-  <p>this is laravel from scratch</p>
+<div class="jumbotron text-center">
+  <h1>Welcome To Laravel!</h1>
+  <p>This is my first laravel application</p>
+  <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+</div>
 @endsection
