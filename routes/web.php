b08b6d29224f0f9fb85219e36ce843a2a6e04cbe
rohan <rohanrajgautam@gmail.com>
2020-09-24 08:59:25 +0545

    create pagecontroller and initial pages setup

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('pages.about');
-});
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Route::get('/about', 'App\Http\Controllers\PagesController@about');
+Route::get('/services', 'App\Http\Controllers\PagesController@services');
 
