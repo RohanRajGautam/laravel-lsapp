4083829bc5e8a5656fa2198b38acd48ad5025169
rohan <rohanrajgautam@gmail.com>
2020-09-25 19:43:54 +0545

    delete data

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg ">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-5">
   <a class="navbar-brand" href="#">{{env('APP_NAME')}}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts" class="btn btn-default">Go Back</a>
+    <a href="/posts" class="btn btn-success mb-3">Go Back</a>
     <h1>{{$post->title}}</h1>
       <div>
         {!! $post->body !!}
       </div>
     <small>Written on {{$post->created_at}}</small>
     <hr>
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+
+    {!!Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method'=> 'POST', 'class'=> 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+
+    {!!Form::close()!!}
 @endsection
