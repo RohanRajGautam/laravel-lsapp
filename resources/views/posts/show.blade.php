59f2914eb2a29ca1ea4c1d544d1b5f5a79e1ac7f
rohan <rohanrajgautam@gmail.com>
2020-09-25 19:25:00 +0545

    update data

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,9 +3,10 @@
 @section('content')
     <a href="/posts" class="btn btn-default">Go Back</a>
     <h1>{{$post->title}}</h1>
-    <div>
-      {!! $post->body !!}
-    </div>
+      <div>
+        {!! $post->body !!}
+      </div>
     <small>Written on {{$post->created_at}}</small>
-    
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 @endsection
